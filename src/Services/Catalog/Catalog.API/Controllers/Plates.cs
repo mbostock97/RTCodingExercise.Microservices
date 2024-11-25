8fa3b0cfb8144493ba93652c7ca3c28f978c4837
Mike Bostock <mikebostock97@gmail.com>
2024-11-25 19:02:50 +0000

    First commit

---
+++ src/Services/Catalog/Catalog.API/Controllers/Plates.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    public class Plates : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
--- src/Web/WebMVC/Controllers/HomeController.cs
+++ src/Web/WebMVC/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace RTCodingExercise.Microservices.Controllers
             return View();
         }
 
+        public IActionResult Plates()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
